bdb711168e3527a83a1ffb1a12e09cb8f2620a23
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2025-05-07 22:33:49 +0200

    Change compiler name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-BIN_DIR="$(dirname "$0")/build/install/compiler/bin"
-$BIN_DIR/compiler "$@"
+BIN_DIR="$(dirname "$0")/build/install/racc0on/bin"
+$BIN_DIR/racc0on "$@"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "compiler" // in case you want to change the name: it is used by run.sh too
+rootProject.name = "racc0on" // in case you want to change the name: it is used by run.sh too
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
