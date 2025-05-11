b54819d368764a6d6dc36b61ac7a38d62c9d6827
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2025-05-11 02:20:48 +0200

    Improve build-demo.sh

--- tests/demo/build-demo.sh
+++ tests/demo/build-demo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-files="$(find -name "*.l1")"
+project_root="$(realpath $(dirname "$0")/../../)"
+demo_dir="$project_root/tests/demo"
+
+if [ ! -d "$demo_dir/bin" ]; then
+  mkdir "$demo_dir/bin"
+fi
+
+files="$(find "$demo_dir" -name "*.l1")"
 IFS=$'\n'
 for file in $files; do
-  "$COMPILER" "$file" "$(dirname "$file")/bin/$(basename "${file%.l1}")"
+  "$project_root/run.sh" "$file" "$(dirname "$file")/bin/$(basename "${file%.l1}")"
 done
