621c6ea71fd9561149096fdf01322582cd4388fc
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2025-05-12 22:39:21 +0200

    Use record instead of class

--- src/main/java/net/rizecookey/racc0on/backend/x86_64/instruction/x8664MovInstr.java
+++ src/main/java/net/rizecookey/racc0on/backend/x86_64/instruction/x8664MovInstr.java
@@ -6,15 +6,7 @@ import net.rizecookey.racc0on.backend.x86_64.storage.x8664StackLocation;
 import net.rizecookey.racc0on.backend.x86_64.storage.x8664StorageLocation;
 import net.rizecookey.racc0on.backend.x86_64.x8664ProcedureGenerator;
 
-public class x8664MovInstr implements x8664Instr {
-    private final x8664StorageLocation to;
-    private final x8664Operand from;
-
-    public x8664MovInstr(x8664StorageLocation to, x8664Operand from) {
-        this.from = from;
-        this.to = to;
-    }
-
+public record x8664MovInstr(x8664StorageLocation to, x8664Operand from) implements x8664Instr {
     @Override
     public void write(x8664ProcedureGenerator generator) {
         if (to instanceof x8664StackLocation && from instanceof x8664StackLocation) {
