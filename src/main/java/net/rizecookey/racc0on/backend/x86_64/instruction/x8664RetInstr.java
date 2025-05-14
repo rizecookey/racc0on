5feeef756518de04382b84b413eb2fb0ba53e70a
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2025-05-14 19:22:24 +0200

    Use record instead of class (again)
    # Conflicts:
    #	src/main/java/net/rizecookey/racc0on/backend/x86_64/instruction/x8664RetInstr.java

--- src/main/java/net/rizecookey/racc0on/backend/x86_64/instruction/x8664RetInstr.java
+++ src/main/java/net/rizecookey/racc0on/backend/x86_64/instruction/x8664RetInstr.java
@@ -4,17 +4,11 @@ import net.rizecookey.racc0on.backend.x86_64.storage.x8664Register;
 import net.rizecookey.racc0on.backend.x86_64.storage.x8664StorageLocation;
 import net.rizecookey.racc0on.backend.x86_64.x8664ProcedureGenerator;
 
-public class x8664RetInstr implements x8664Instr {
-    private final x8664StorageLocation returnLoc;
-
-    public x8664RetInstr(x8664StorageLocation returnLocation) {
-        returnLoc = returnLocation;
-    }
-
+public record x8664RetInstr(x8664StorageLocation returnLocation) implements x8664Instr {
     @Override
     public void write(x8664ProcedureGenerator generator) {
-        if (!returnLoc.equals(x8664Register.RAX)) {
-            new x8664MovInstr(x8664Register.RAX, returnLoc).write(generator);
+        if (!returnLocation.equals(x8664Register.RAX)) {
+            new x8664MovInstr(x8664Register.RAX, returnLocation).write(generator);
         }
 
         generator.tearDownStack();
