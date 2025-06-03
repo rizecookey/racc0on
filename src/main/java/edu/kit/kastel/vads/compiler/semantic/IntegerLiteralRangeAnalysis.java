5b81b6d2890bc05fcdde93fb62f3b0f081479624
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2025-06-03 20:04:29 +0200

    Include RecursivePostorderVisitor wrapping in IntegerLiteralRangeAnalysis

--- src/main/java/edu/kit/kastel/vads/compiler/semantic/IntegerLiteralRangeAnalysis.java
+++ src/main/java/edu/kit/kastel/vads/compiler/semantic/IntegerLiteralRangeAnalysis.java
@@ -2,16 +2,21 @@ package edu.kit.kastel.vads.compiler.semantic;
 
 import edu.kit.kastel.vads.compiler.parser.ast.IntLiteralTree;
 import edu.kit.kastel.vads.compiler.parser.visitor.NoOpVisitor;
+import edu.kit.kastel.vads.compiler.parser.visitor.RecursivePostorderVisitor;
 import edu.kit.kastel.vads.compiler.parser.visitor.Unit;
 
-public class IntegerLiteralRangeAnalysis implements NoOpVisitor<Namespace<Void>> {
+public class IntegerLiteralRangeAnalysis extends RecursivePostorderVisitor<Namespace<Void>, Unit> {
 
-    @Override
-    public Unit visit(IntLiteralTree intLiteralTree, Namespace<Void> data) {
-      intLiteralTree.parseValue()
-          .orElseThrow(
-              () -> new SemanticException(intLiteralTree.span(), "invalid integer literal " + intLiteralTree.value())
-          );
-        return NoOpVisitor.super.visit(intLiteralTree, data);
+    public IntegerLiteralRangeAnalysis() {
+        super(new NoOpVisitor<>() {
+            @Override
+            public Unit visit(IntLiteralTree intLiteralTree, Namespace<Void> data) {
+                intLiteralTree.parseValue()
+                        .orElseThrow(
+                                () -> new SemanticException(intLiteralTree.span(), "invalid integer literal " + intLiteralTree.value())
+                        );
+                return NoOpVisitor.super.visit(intLiteralTree, data);
+            }
+        });
     }
 }
--- src/main/java/edu/kit/kastel/vads/compiler/semantic/SemanticAnalysis.java
+++ src/main/java/edu/kit/kastel/vads/compiler/semantic/SemanticAnalysis.java
@@ -12,7 +12,7 @@ public class SemanticAnalysis {
     }
 
     public void analyze() {
-        this.program.accept(new RecursivePostorderVisitor<>(new IntegerLiteralRangeAnalysis()), new Namespace<>());
+        this.program.accept(new IntegerLiteralRangeAnalysis(), new Namespace<>());
         this.program.accept(new RecursivePostorderVisitor<>(new VariableStatusAnalysis()), new Namespace<>());
         this.program.accept(new ReturnAnalysis(), new ReturnAnalysis.ReturnState());
     }
