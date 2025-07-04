c9d73c516b7e7cee6d15eef84940cc808d7acf3c
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2025-07-04 20:09:38 +0200

    Return correct name for StructType asString()

--- src/main/java/net/rizecookey/racc0on/parser/type/StructType.java
+++ src/main/java/net/rizecookey/racc0on/parser/type/StructType.java
@@ -5,6 +5,6 @@ import net.rizecookey.racc0on.lexer.Identifier;
 public record StructType(Identifier name) implements Type {
     @Override
     public String asString() {
-        return "struct " + name();
+        return "struct " + name().asString();
     }
 }
